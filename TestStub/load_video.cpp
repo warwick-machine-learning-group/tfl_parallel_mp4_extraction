3a54e56b1ddaee147ba1352e09ae2e0479eeff37
diag-toolkits <chuah_edward@yahoo.com>
2020-05-19 13:07:13 +0100

    Add files via upload

--- TestStub/load_video.cpp
+++ TestStub/load_video.cpp
@@ -25,11 +25,11 @@ IntT main( IntT argc, CharT *argv[] )
 	if( argc == 1 )
 	{
 		cout << "Video data loader\n"
-				"1. To execute on a single processor, specify tflvidext -single "
+				"1. To execute on a single processor, specify vidext -single "
 				"[video_source_directory] [video_destination_directory]\n"
 				"2. To execute on multiple processors, specify mpirun -np "
-				"[number_of_processors] tflta -multiple [video_source_directory]"
-				" [video_destination_directory]\n";
+				"[number_of_processors] vidext -multiple [number of procesors] "
+				"[video_source_directory] [video_destination_directory]\n";
 		return -1;
 	}
 
@@ -48,9 +48,38 @@ IntT main( IntT argc, CharT *argv[] )
 		return 0;
 	}
 
-	if( arguments.at( 0 ).compare( "-multiple" ) == 0 && arguments.size() == 3 )
+	if( arguments.at( 0 ).compare( "-multiple" ) == 0 && arguments.size() == 4 )
 	{
-		video_loader.run_on_4_processors( arguments.at( 1 ), arguments.at( 2 ) );
+		IntT number_of_processors = atoi( arguments.at( 1 ).c_str() );
+		IntVectorT videos_frames_count_list;
+		UCharVectorT videos_frames_data;
+		if( number_of_processors == 4 )
+		{
+			video_loader.run_on_4_processors( arguments.at( 2 ), arguments.at( 3 ) );
+			video_loader.get_videos_frames( videos_frames_count_list, videos_frames_data );
+			cout << "Number of videos: " << videos_frames_count_list.size() << "\n";
+			cout << "Number of frames elements: " << videos_frames_data.size() << "\n";
+			return 0;
+		}
+
+		if( number_of_processors == 8 )
+		{
+			video_loader.run_on_8_processors( arguments.at( 2 ), arguments.at( 3 ) );
+			video_loader.get_videos_frames( videos_frames_count_list, videos_frames_data );
+			cout << "Number of videos: " << videos_frames_count_list.size() << "\n";
+			cout << "Number of frames elements: " << videos_frames_data.size() << "\n";
+			return 0;
+		}
+
+		if( number_of_processors == 16 )
+		{
+			video_loader.run_on_16_processors( arguments.at( 2 ), arguments.at( 3 ) );
+			video_loader.get_videos_frames( videos_frames_count_list, videos_frames_data );
+			cout << "Number of videos: " << videos_frames_count_list.size() << "\n";
+			cout << "Number of frames elements: " << videos_frames_data.size() << "\n";
+			return 0;
+		}
+
 		return 0;
 	}
 
